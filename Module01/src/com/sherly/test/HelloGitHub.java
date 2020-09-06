d47630aedcf3d5ef5f9cceaebffcd0bb04f3fe54
SherlyStart <756139338@qq.com>
2020-09-06 16:33:56 +0800

    Initial commit

---
+++ Module01/src/com/sherly/test/HelloGitHub.java
@@ -0,0 +1,12 @@
+package com.sherly.test;
+
+/**
+ * @author Sherly
+ * @date 2020/9/6 - 16:32
+ * @func. :
+ */
+public class HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub !");
+    }
+}
